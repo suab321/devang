efdf10ced28db0df1e9eb69951d339ee860659f1
Abhinav Singh <adityasaha890@gmail.com>
2019-01-07 19:13:56 +0530

    sf

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Md5} from 'ts-md5/dist/md5';
 
 @Component({
   selector: 'app-register',
@@ -10,6 +11,31 @@ export class RegisterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var name=document.getElementById("name");
+    var email=document.getElementById("email");
+    var password=document.getElementById("password");
+    var cpassword=document.getElementById("cpassword");
+
+    name.onpaste=function(e){e.preventDefault()}
+    email.onpaste=function(e){e.preventDefault()}
+    password.onpaste=function(e){e.preventDefault()}
+    cpassword.onpaste=function(e){e.preventDefault()}
+
+    name.oncopy=function(e){e.preventDefault()}
+    email.oncopy=function(e){e.preventDefault()}
+    password.oncopy=function(e){e.preventDefault()}
+    cpassword.oncopy=function(e){e.preventDefault()}
+
+    name.oncut=function(e){e.preventDefault()}
+    email.oncut=function(e){e.preventDefault()}
+    password.oncut=function(e){e.preventDefault()}
+    cpassword.oncut=function(e){e.preventDefault()}
+  }
+
+  submit(e){
+    const hash=new Md5();
+    console.log(hash.appendStr(e.value).end());
+
   }
 
 }
